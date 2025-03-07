b1b21e5ef927bd255a4f06d9a17989254d9fb3b5
Caryn Carter <carter.caryn@gmail.com>
2025-03-07 17:10:04 -0600

    added to cdk-stack.ts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
@@ -7,6 +8,17 @@ export class CdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const destinationBucket = new s3.Bucket(this, "DestinationBucket", {
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      versioned: false,
+      websiteErrorDocument: "index.html",
+      websiteIndexDocument: "index.html",
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
