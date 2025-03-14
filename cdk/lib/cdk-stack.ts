c618a74969e0b753f7e09d2199301007f925ee42
Caryn Carter <carter.caryn@gmail.com>
2025-03-14 17:04:42 -0500

    adding to cdk-stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_certificatemanager as acm } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { HostedZone } from 'aws-cdk-lib/aws-route53';
@@ -14,16 +15,21 @@ export class CdkStack extends cdk.Stack {
       accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
       autoDeleteObjects: true,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
-      publicReadAccess: true,
+      //publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       versioned: false,
-      websiteErrorDocument: "index.html",
-      websiteIndexDocument: "index.html",
+      //websiteErrorDocument: "index.html",
+      //websiteIndexDocument: "index.html",
     });
 
     const hostedZone = new HostedZone (this, "isAwesomeHostedZone",  {
       zoneName: 'forkalicious.isawesome.xyz'
     })
+
+    const certificate = new acm.Certificate(this, "Certificate", {
+      domainName: "forkalicious.isawesome.xyz",
+      validation: acm.CertificateValidation.fromDns(),
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkQueue', {
