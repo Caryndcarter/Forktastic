13ee810c7ee7538b8f57a13c727144ae824e8ba4
Daniel <danstraws@gmail.com>
2025-03-17 11:56:04 -0400

    changed local storage ID ref to be DRY: created once at top of page

--- client/src/utils_graphQL/localStorageService.ts
+++ client/src/utils_graphQL/localStorageService.ts
@@ -17,15 +17,19 @@ const defaultRecipe: RecipeDetails = {
   spoonacularId: 0,
 };
 
+const currentRecipeID = "currentRecipe";
+const accountDietID = "Dietary Needs";
+const queryID = "Query";
+
 // holds logic for managing variables in local storage
 class LocalStorageService {
   setCurrentRecipe(recipe: RecipeDetails) {
     const stringyRecipe = JSON.stringify(recipe);
-    localStorage.setItem("currentRecipe", stringyRecipe);
+    localStorage.setItem(currentRecipeID, stringyRecipe);
   }
 
   getCurrentRecipe(): RecipeDetails {
-    const stringyRecipe = localStorage.getItem("currentRecipe");
+    const stringyRecipe = localStorage.getItem(currentRecipeID);
 
     if (!stringyRecipe) {
       return defaultRecipe;
@@ -36,11 +40,11 @@ class LocalStorageService {
 
   setAccountDiet(dietaryNeeds: DietaryNeeds): void {
     const stringyDiet = JSON.stringify(dietaryNeeds);
-    localStorage.setItem("Dietary Needs", stringyDiet);
+    localStorage.setItem(accountDietID, stringyDiet);
   }
 
   getAccountDiet(): DietaryNeeds {
-    const stringyDiet = localStorage.getItem("Dietary Needs");
+    const stringyDiet = localStorage.getItem(accountDietID);
 
     if (!stringyDiet) {
       return { diet: "", intolerances: [] };
@@ -50,11 +54,11 @@ class LocalStorageService {
   }
 
   setQuery(query: string) {
-    localStorage.setItem("query", query);
+    localStorage.setItem(queryID, query);
   }
 
   getQuery() {
-    const query = localStorage.getItem("query");
+    const query = localStorage.getItem(queryID);
 
     if (!query) {
       return "";
